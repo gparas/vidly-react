13ddfbef09080770285e895fc37ab57ec7be9b04
georgios.parapanissios <georgios.parapanissios@marinetraffic.com>
2020-11-06 22:05:47 +0200

    first commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+  state = {
+    movies: [],
+  };
+  componentDidMount() {
+    this.setState({ movies: getMovies() });
+  }
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    const { movies } = this.state;
+    if (movies.length === 0) return <p>There are no movies in the database</p>;
+    return (
+      <>
+        <p>Showing {movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col" colSpan={2}>
+                Rate
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <th scope="row">{movie.title}</th>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
